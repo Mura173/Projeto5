1dda162e468f2ca06bb5755708e44c33e1595874
Murilo Ângelo <murapb07@gmail.com>
2025-09-22 22:50:47 -0300

    finalback

--- src/Server/src/db.js
+++ src/Server/src/db.js
@@ -2,13 +2,13 @@ import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
 
+const isRender = process.env.PG_HOST?.includes("render.com");
+
 export const pool = new Pool({
   host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DB,
-  port: process.env.PG_PORT,
-  ssl: process.env.PG_HOST?.includes("render.com")
-    ? { rejectUnauthorized: false } // necessário no Render
-    : false,
+  user: String(process.env.PG_USER),
+  password: String(process.env.PG_PASSWORD),
+  database: String(process.env.PG_DB),
+  port: Number(process.env.PG_PORT),
+  ssl: isRender ? { rejectUnauthorized: false } : false,
 });
