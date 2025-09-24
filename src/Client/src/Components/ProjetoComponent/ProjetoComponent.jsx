aa7c47dd7a8922a6b89cfe6ecb3bbc8acf074a78
25027958 <25027958@fecap.br>
2025-09-24 11:02:21 -0300

    Estrutura do componente do projeto

---
+++ src/Client/src/Components/ProjetoComponent/ProjetoComponent.jsx
@@ -0,0 +1,19 @@
+import "./ProjetoComponent.css"
+
+function ProjetoComponent(){
+    return(
+        <>
+        <header className="projeto-component">
+        <div className="group"></div>
+        
+        <div className="start-date"></div>
+        
+        <div className="mentor"></div>
+
+        <div className="arrecadacao"></div>
+        </header>
+        </>
+    )
+}
+
+export default ProjetoComponent
---
+++ src/Client/src/Routes/MeusProjetos/MeusProjetos.jsx
@@ -0,0 +1,15 @@
+import "./MeusProjetos.css"
+
+import HeaderComponent from "../../Components/Header/Header"
+import ProjetoComponent from "../../Components/ProjetoComponent/ProjetoComponent"
+
+function MeusProjetos(){
+    return(
+        <>
+        <HeaderComponent />
+        <ProjetoComponent />
+        </>
+    )
+}
+
+export default MeusProjetos
