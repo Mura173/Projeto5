eb713ce08fe93c5535125e1720d6b1c06eb966e0
25027958 <25027958@fecap.br>
2025-09-26 09:08:25 -0300

    ajustes

--- src/Client/src/Components/ProjetoComponent/ProjetoComponent.jsx
+++ src/Client/src/Components/ProjetoComponent/ProjetoComponent.jsx
@@ -4,13 +4,24 @@ function ProjetoComponent(){
     return(
         <>
         <header className="projeto-component">
-        <div className="group"></div>
-        
-        <div className="start-date"></div>
-        
-        <div className="mentor"></div>
+            <div className="header-container">
+                <div className="group">
+                    <a href="#">Grupo 2 - 2025</a>
+                </div>
+                <div className="grupo-info">
+                    <p className="title-info">Data de início</p>
+                    <p className="title-project">10/03/2025</p>
+                </div>
+                <div className="grupo-info">
+                    <p className="title-info">Mentor</p>
+                    <p className="title-project">Fulano</p>
+                </div>
 
-        <div className="arrecadacao"></div>
+                <div className="grupo-info">
+                    <p className="title-info">Arrecadação</p>
+                    <p className="title-project">20kg/R$400</p>
+                </div>
+            </div>
         </header>
         </>
     )
