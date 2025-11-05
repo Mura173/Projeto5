1f4ed0f889115124e234bb03ffc282cefa52e1c8
25027808 <harry.zhu2016@gmail.com>
2025-11-05 10:41:27 -0300

    rotas links menu

--- src/Client/src/Components/Menu/Menu.jsx
+++ src/Client/src/Components/Menu/Menu.jsx
@@ -1,18 +1,43 @@
+import { NavLink, useLocation } from "react-router-dom";
 import "./Menu.css";
 function MenuComponent({menuStatus}){
+    const location = useLocation();
+    function closeNavButton(currentPage){
+        return (location.pathname === currentPage ? 'closed' : '')
+    }
+
     return(
         <div className="container-menu"> 
         <div className={menuStatus === false ? "closed" : "open"}>
-            <a href="">Meu perfil</a>
-            <hr />
-            <a href="">Meus projetos</a>
-            <hr />
-            <a href="">Home</a>
-            <hr />
-            <a href="">Logout</a>
+            <NavLink to={'/perfil'} 
+                className={closeNavButton('/perfil')}>
+                    Meu perfil
+            </NavLink>
+            <hr className={closeNavButton('/perfil')}/>
+
+            <NavLink to={'/meus-projetos'} 
+                className={closeNavButton('/meus-projetos')}>
+                    Meus projetos
+            </NavLink>
+            <hr className={closeNavButton('/meus-projetos')}/>
+
+            <NavLink to={'/home'} 
+                className={closeNavButton('/home')}>
+                    Home
+            </NavLink>
+            <hr className={closeNavButton('/home')}/>
+
+            <NavLink to={'/login'} className={''}>Logout</NavLink>
         </div>
         </div>
     )
 }
+
+{location.pathname !== '/perfil' && (
+    <>
+        <NavLink to={'/perfil'}>Meu perfil</NavLink>
+        <hr />
+    </>
+)}
 
 export default MenuComponent
